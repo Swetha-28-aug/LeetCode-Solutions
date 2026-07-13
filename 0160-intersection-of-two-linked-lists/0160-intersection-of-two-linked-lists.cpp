59179035d928f23259446240ab9871871d7f3d5e
Swetha-28-aug <swethanishanth321@gmail.com>
2026-07-13 11:56:39 +0530

    Time: 53 ms (77.94%), Space: 23.9 MB (56.97%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -1,28 +1,33 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
+    int length(ListNode *head){
+        int len=0;
+        while(head!=NULL){
+            head=head->next;
+            len++;
+        }
+        return len;
+    }
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode *tempA=headA;
-        ListNode *tempB=headB;
-        while(tempA!=tempB){
-            if(tempA==NULL){
-                tempA=headB;
-            }else{
-                tempA=tempA->next;
+ ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    int lenA=length(headA);
+    int lenB=length(headB);
+    int diff=abs(lenA-lenB);
+        if(lenA>lenB){
+            while(diff--){
+                headA=headA->next;
             }
-            if(tempB==NULL){
-                tempB=headA;
-            }else{
-                tempB=tempB->next;
+        }else{
+            while(diff--){
+                headB=headB->next;
             }
         }
-        return tempA;
+    while(headA!=NULL){
+        if(headA==headB){
+            return headA;
+        }
+        headA=headA->next;
+        headB=headB->next;
+    }
+    return NULL;
     }
 };
