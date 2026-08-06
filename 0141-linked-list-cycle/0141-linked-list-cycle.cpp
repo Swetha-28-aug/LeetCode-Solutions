f43a5378d4167db52071f1317844193b0002933a
Swetha-28-aug <swethanishanth321@gmail.com>
2026-08-06 10:54:11 +0530

    Time: 6 ms (92.01%), Space: 11.8 MB (54.54%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -11,11 +11,13 @@ public:
     bool hasCycle(ListNode *head) {
       ListNode *fast=head;
       ListNode *slow=head;
-      while(fast != NULL && fast->next!=NULL){
+      while(fast!=NULL && fast->next!=NULL){
         slow=slow->next;
         fast=fast->next->next;
-        if(slow==fast) return true;
+        if(fast==slow){
+            return true;
+        }
       }
-     return false;  
+      return false;
     }
 };
